55149307f8694733c31ceb5f2dea8b95dce97c47
Abdulkadir Tuncay <abtuncay@innova.com.tr>
2018-06-20 11:31:09 +0300

    commit

--- Deneme/Adim_1.js
+++ Deneme/Adim_1.js
@@ -1,1 +1,2 @@
-console.log("Merhaba Kadir!! Ne ayaksın becerdin heralde yürü beaaaaa11")
+console.log("Merhaba Kadir!! Ne ayaksın becerdin heralde yürü beaaaaa11");
+console.log("Merhaba Kadir!! Ne ayaksın becerdin heralde yürü beaaaaa11");
